6a547fdca7b015b17617c9172e0a1ccb2ff36b98
taaaniel <kaliszewskiwojtek@gmail.com>
2019-01-02 21:58:14 +0100

    Add battlegroup array.

--- src/app/dashboard/army-group/army-group.component.ts
+++ src/app/dashboard/army-group/army-group.component.ts
@@ -10,15 +10,23 @@ import { CreateRosterService } from '../../_services/create-roster.service';
 export class ArmyGroupComponent implements OnInit {
 
   groupName: string;
+  battlegroupType;
 
   constructor(
     private createRosterService: CreateRosterService,
   ) { }
 
   ngOnInit() {
+    this.battlegroupType = new Array();
     this.createRosterService.groupName$.subscribe((groupName) => {
       this.groupName = groupName;
+      this.createBattleGroups(groupName);
     });
+  }
+
+  createBattleGroups(groupName) {
+    this.battlegroupType.push(groupName);
+      console.log("this.battlegroupType", this.battlegroupType, groupName);
   }
 
 }
